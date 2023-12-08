6e49227d573a304add2dbc137762f41d9b449436
theniceguy8731 <kushagra22263@iiitd.ac.in>
2023-12-09 04:01:20 +0530

    update

--- src/main/java/Classes/Game.java
+++ src/main/java/Classes/Game.java
@@ -1,6 +1,14 @@
 package Classes;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class Game {
     Player currplayer;
@@ -28,4 +36,17 @@ public class Game {
 
     public void resume(){}
 
+    public static class Main extends Application {
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage stage) throws IOException {
+            Parent root= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../resources/Home.fxml")));
+            stage.setTitle("Stick Hero");
+            stage.setScene(new Scene(root,335,600));
+            stage.show();
+        }
+    }
 }
