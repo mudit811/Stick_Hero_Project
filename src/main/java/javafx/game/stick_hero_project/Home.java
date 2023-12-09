ffa2c14adbfed495869435269d61497e17643b3a
theniceguy8731 <kushagra22263@iiitd.ac.in>
2023-12-09 16:42:58 +0530

    connected home buttons

--- src/main/java/javafx/game/stick_hero_project/Home.java
+++ src/main/java/javafx/game/stick_hero_project/Home.java
@@ -35,10 +35,40 @@ public class Home {
         scene=new Scene(root);
         stage.setScene(scene);
         GameController.gameLogic.pillar_setup();
-
         scene.setOnKeyPressed(GameController.gameLogic::keyboard_mapper);
         scene.setOnKeyReleased(GameController.gameLogic::keyboard_release);
         stage.show();
     }
+    public void loadShop(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+        Scene scene;
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Shop.fxml")));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void loadAbout(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+        Scene scene;
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("About.fxml")));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void loadColors(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+        Scene scene;
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Colors.fxml")));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 }
